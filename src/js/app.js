0ada42b1a9ce867901dd2f13d8a53101753a9d74
Roman T <rt.hagbo@gmail.com>
2022-02-20 05:06:45 +0500

    Дз №6 - «Object, Reflection и Proxy» - Destructuring

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 import orderByProps from './modules/orderbyprops';
+import destructuring from './modules/destructuring';
 
+/* ПРОВЕРКА ЗПЕРВОГО ЗАДАНИЯ */
 const swordsman = {
   name: 'мечник',
   health: 10,
@@ -12,3 +14,29 @@ const swordsman = {
 const sort = ['name', 'level'];
 
 console.log(orderByProps(swordsman, sort));
+
+/* ПРОВЕРКА ВТОРОГО ЗАДАНИЯ */
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+console.log(destructuring(character));
